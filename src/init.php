d0c065881d5d161028473b6e96292cfa92b66bd8
lucasbg98 <99032333+lucasbg98@users.noreply.github.com>
2023-09-12 10:19:50 -0300

    CD02 Backend

---
+++ src/init.php
@@ -0,0 +1,17 @@
+<?php
+
+//timezone
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// conexão com o banco de dados
+define('BD_SERVIDOR','localhost');
+define('BD_USUARIO','root');
+define('BD_SENHA','123456');
+define('BD_BANCO','');
+define('BD_PORTA','3306');
+
+
+
+
+?>

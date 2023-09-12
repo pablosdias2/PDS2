9891073cbee8da41ab891097edece96c405fbd44
Lucas <lucasbg98@hotmail.com>
2023-09-12 11:35:50 -0300

    alteracao init.php

--- src/init.php
+++ src/init.php
@@ -8,7 +8,7 @@ date_default_timezone_set('America/Sao_Paulo');
 define('BD_SERVIDOR','localhost');
 define('BD_USUARIO','root');
 define('BD_SENHA','123456');
-define('BD_BANCO','');
+define('BD_BANCO','infootball');
 define('BD_PORTA','3306');
 
 

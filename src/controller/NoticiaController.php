c237413850a95c2d6ad4060ac4bc2f8e29f5d823
Lucas <lucasbg98@hotmail.com>
2023-10-30 15:48:49 -0300

    alterar noticia / buscar noticia pelo id

--- src/controller/NoticiaController.php
+++ src/controller/NoticiaController.php
@@ -14,25 +14,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     if (!isset($_POST["titulo"])) {
         http_response_code(400);
-        echo json_encode(array("message" => "Campo faltoso: 'email'"));
+        echo json_encode(array("message" => "Campo faltoso: 'titulo'"));
         return;
     }
 
     if (!isset($_POST["descricao"])) {
         http_response_code(400);
-        echo json_encode(array("message" => "Campo faltoso: 'senha'"));
+        echo json_encode(array("message" => "Campo faltoso: 'descricao'"));
         return;
     }
 
     if (!isset($_POST["texto"])) {
         http_response_code(400);
-        echo json_encode(array("message" => "Campo faltoso: 'nome'"));
+        echo json_encode(array("message" => "Campo faltoso: 'texto'"));
         return;
     }
 
     if (!isset($_POST["autor"])) {
         http_response_code(400);
-        echo json_encode(array("message" => "Campo faltoso: 'username'"));
+        echo json_encode(array("message" => "Campo faltoso: 'autor'"));
         return;
     }
 
@@ -46,27 +46,87 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         echo json_encode(array("message" => "Erro ao criar nova noticia"), JSON_UNESCAPED_UNICODE);
         return;
     }
+}
 
+if ($_SERVER["REQUEST_METHOD"] === "PUT") {
+
+    $json = (array) json_decode(file_get_contents("php://input"));
+    $_PUT = $json;
+
+    if (!isset($_PUT["id"])) {
+        http_response_code(400);
+        echo json_encode(array("message" => "Campo faltoso: 'id'"));
+        return;
+    }
+
+    if (!isset($_PUT["titulo"])) {
+        http_response_code(400);
+        echo json_encode(array("message" => "Campo faltoso: 'titulo'"));
+        return;
+    }
+
+    if (!isset($_PUT["descricao"])) {
+        http_response_code(400);
+        echo json_encode(array("message" => "Campo faltoso: 'descricao'"));
+        return;
+    }
+
+    if (!isset($_PUT["texto"])) {
+        http_response_code(400);
+        echo json_encode(array("message" => "Campo faltoso: 'texto'"));
+        return;
+    }
+
+    if (!isset($_PUT["autor"])) {
+        http_response_code(400);
+        echo json_encode(array("message" => "Campo faltoso: 'autor'"));
+        return;
+    }
+
+    $noticia = NoticiaService::getById($_PUT["id"]);
+
+    if (isset($noticia)) {
+        if ($noticia->getId() !== $_PUT["id"]) {
+            http_response_code(401);
+
+            echo json_encode(array("message" => "Proibido atualizar uma noticia terceira"));
+            return;
+        }
+
+        $updatedNoticia = NoticiaService::updateNoticia($_PUT);
+
+        if (isset($updatedNoticia)) {
+            echo json_encode($updatedNoticia, JSON_UNESCAPED_UNICODE);
+            return;
+        } else {
+            http_response_code(500);
+            echo json_encode(array("message" => "Erro ao atualizar a noticia"), JSON_UNESCAPED_UNICODE);
+            return;
+        }
+    } else {
+        http_response_code(404);
+        echo json_encode(array("message" => "Noticia inexistente"));
+        return;
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
     parse_str(file_get_contents('php://input'), $_DELETE);
 
-    if(!isset($_DELETE["id"])){
+    if (!isset($_DELETE["id"])) {
         http_response_code(400);
-    echo json_encode(array("message" => "Campo faltoso: 'id'"));
-    return;
+        echo json_encode(array("message" => "Campo faltoso: 'id'"));
+        return;
     }
 
-   $result = NoticiaService::deleteNoticia($_DELETE);
+    $result = NoticiaService::deleteNoticia($_DELETE);
 
-   if (isset($result)) {
-    echo json_encode($result, JSON_UNESCAPED_UNICODE);
-    return;
-  } else {
-    http_response_code(500);
-    echo json_encode(array("message" => "Erro ao deletar usuario"), JSON_UNESCAPED_UNICODE);
-    return;
-  }
-
+    if (isset($result)) {
+        echo json_encode($result, JSON_UNESCAPED_UNICODE);
+        return;
+    } else {
+        http_response_code(500);
+        echo json_encode(array("message" => "Erro ao deletar noticia"), JSON_UNESCAPED_UNICODE);
+        return;
+    }
 }
--- src/service/NoticiaService.php
+++ src/service/NoticiaService.php
@@ -13,9 +13,20 @@ class NoticiaService
         return NoticiaDAO::create($noticia);
     }
 
+    public static function updateNoticia($args)
+    {
+        $noticia = Noticia::completeObject($args);
+
+        return isset($noticia) ? NoticiaDAO::update(array($noticia)) : null;
+    }
+
     public static function deleteNoticia($args)
     {
         return isset($args) ? NoticiaDAO::delete($args["id"]) : null;
     }
 
+    public static function getById(string $id): ?Noticia
+    {
+        return isset($id) ? NoticiaDAO::read(array("getById", $id)) : null;
+    }
 }
